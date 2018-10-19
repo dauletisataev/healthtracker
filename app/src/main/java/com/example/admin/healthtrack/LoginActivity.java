b24e8c3655b653ddda40b75e44760f98b1f59503
Timur <timon76ers@gmail.com>
2018-10-19 15:07:10 +0600

    Изменил стили и добавил LoginActivity

--- app/src/main/java/com/example/admin/healthtrack/LoginActivity.java
+++ app/src/main/java/com/example/admin/healthtrack/LoginActivity.java
@@ -1,16 +1,16 @@
 package com.example.admin.healthtrack;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
-        startActivity(new Intent(this, LoginActivity.class));
+
     }
 }
